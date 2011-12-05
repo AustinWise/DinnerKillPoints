453c5fcfb5cba685000e9327a3299533dc7e9f90
Austin Wise <AustinWise@gmail.com>
2011-12-04 20:11:25 -0800

    Updated to use a database.

---
+++ DinnerKillPoints/Dkp.cs
@@ -0,0 +1,31 @@
+namespace DinnerKillPoints
+{
+    partial class DkpDataContext
+    {
+    }
+
+    partial class Transaction
+    {
+    }
+
+    partial class Person
+    {
+        public override string ToString()
+        {
+            return FirstName + " " + LastName;
+        }
+
+        public override int GetHashCode()
+        {
+            return this.ID;
+        }
+
+        public override bool Equals(object obj)
+        {
+            var other = obj as Person;
+            if (other == null)
+                return false;
+            return other.ID == this.ID;
+        }
+    }
+}
--- DinnerKillPoints/Program.cs
+++ DinnerKillPoints/Program.cs
@@ -8,15 +8,6 @@ namespace DinnerKillPoints
     //Future GUI features:
     // * Let someone pay off another person's debt.
     // * A nice way to split a bill easily.
-    class Person
-    {
-        public string Name { get; set; }
-
-        public override string ToString()
-        {
-            return Name;
-        }
-    }
 
     class Debt
     {
@@ -25,40 +16,23 @@ namespace DinnerKillPoints
         public Person Debtor { get; set; }
     }
 
-    class Transaction : Debt
-    {
-        public string Desc { get; set; }
-        public DateTime When { get; set; }
-    }
-
     class Program
     {
         static void Main(string[] args)
         {
-            Person austin, david, caspar, wesley, maria;
-            var people = new List<Person>();
-            people.Add(austin = new Person() { Name = "Austin" });
-            people.Add(david = new Person() { Name = "David" });
-            people.Add(caspar = new Person() { Name = "Caspar" });
-            people.Add(wesley = new Person() { Name = "Wesley" });
-            people.Add(maria = new Person() { Name = "Maria" });
+            var db = new DkpDataContext();
+            var people = db.People.ToArray();
 
-            var register = new List<Transaction>();
-            register.Add(new Transaction() { Amount = 4450, Debtor = wesley, Creditor = austin, Desc = "Limon", When = new DateTime(2011, 11, 1) });
-            register.Add(new Transaction() { Amount = 550, Debtor = austin, Creditor = caspar, Desc = "Limon", When = new DateTime(2011, 11, 1) });
-            register.Add(new Transaction() { Amount = 6100, Debtor = david, Creditor = austin, Desc = "Limon", When = new DateTime(2011, 11, 1) });
-            register.Add(new Transaction() { Amount = 3450, Debtor = maria, Creditor = austin, Desc = "Limon", When = new DateTime(2011, 11, 1) });
-
-            register.Add(new Transaction() { Amount = 3000, Debtor = wesley, Creditor = david, Desc = "Fiesta", When = new DateTime(2011, 12, 1) });
-            register.Add(new Transaction() { Amount = 2300, Debtor = caspar, Creditor = david, Desc = "Fiesta", When = new DateTime(2011, 12, 1) });
-            register.Add(new Transaction() { Amount = 2800, Debtor = austin, Creditor = david, Desc = "Fiesta", When = new DateTime(2011, 12, 1) });
-
-            //fake data follows
-            //register.Add(new Transaction() { Amount = 6666, Debtor = david, Creditor = austin, Desc = "Fiesta", When = new DateTime(2011, 12, 15) });
+            Console.WriteLine("Raw tranactions:");
+            foreach (var t in db.Transactions.OrderBy(t => t.Created))
+            {
+                Console.WriteLine("\t{0} -> {1}: {2}", t.Debtor, t.Creditor, t.Amount);
+            }
+            Console.WriteLine();
 
             //sum all debts from one person to another
             var summedDebts = new Dictionary<Tuple<Person, Person>, int>();
-            foreach (var t in register)
+            foreach (var t in db.Transactions)
             {
                 var tup = new Tuple<Person, Person>(t.Debtor, t.Creditor);
                 int net = 0;
@@ -78,10 +52,10 @@ namespace DinnerKillPoints
             //net up all the debts
             //what's this, O(2n) ?
             var netMoney = new List<Debt>();
-            for (int i = 0; i < people.Count - 1; i++)
+            for (int i = 0; i < people.Length - 1; i++)
             {
                 var p1 = people[i];
-                for (int j = i + 1; j < people.Count; j++)
+                for (int j = i + 1; j < people.Length; j++)
                 {
                     var p2 = people[j];
                     var net = 0;
@@ -114,6 +88,9 @@ namespace DinnerKillPoints
                 Console.WriteLine("\t{0}: {1}", tup.Debtor, tup.Amount);
             }
             Console.WriteLine();
+
+
+            db.Dispose();
         }
     }
 }
