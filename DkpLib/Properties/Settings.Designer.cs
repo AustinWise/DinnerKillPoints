f53c5c6174249d5d6e1382a3a65933e6060dd9c3
Austin Wise <AustinWise@gmail.com>
2012-06-14 22:12:32 -0700

    Updated SQL location to match new location.

--- DkpLib/Properties/Settings.Designer.cs
+++ DkpLib/Properties/Settings.Designer.cs
@@ -26,7 +26,7 @@ namespace Austin.DkpLib.Properties {
         [global::System.Configuration.ApplicationScopedSettingAttribute()]
         [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
         [global::System.Configuration.SpecialSettingAttribute(global::System.Configuration.SpecialSetting.ConnectionString)]
-        [global::System.Configuration.DefaultSettingValueAttribute("Data Source=.;Initial Catalog=DKP;Integrated Security=True")]
+        [global::System.Configuration.DefaultSettingValueAttribute("Data Source=mssqloltp.wise.local;Initial Catalog=DKP;Integrated Security=True")]
         public string DKPConnectionString {
             get {
                 return ((string)(this["DKPConnectionString"]));
