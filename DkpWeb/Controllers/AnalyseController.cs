663b5b9aaa1db66afb3612dda14929c8c5f49bf9
Austin Wise <AustinWise@gmail.com>
2012-10-21 20:39:31 -0700

    Basic analyse display.

---
+++ DkpWeb/Controllers/AnalyseController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Austin.DkpLib;
+
+namespace DkpWeb.Controllers
+{
+    public class AnalyseController : Controller
+    {
+        //
+        // GET: /Analyse/
+
+        public ActionResult Index()
+        {
+            Person[] people;
+            using (var dc = new DkpDataContext())
+            {
+                people = dc.People.ToArray();
+            }
+            return View(people);
+        }
+
+        public ActionResult Display()
+        {
+            return View();
+        }
+    }
+}
