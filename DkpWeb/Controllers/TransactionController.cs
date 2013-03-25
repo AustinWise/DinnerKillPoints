0c6a8a2de9deec21428ff4864638cb544bdd9b53
Austin Wise <AustinWise@gmail.com>
2013-03-24 18:31:02 -0700

    Added the ability to add new transactions.

--- DkpWeb/Controllers/TransactionController.cs
+++ DkpWeb/Controllers/TransactionController.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using Austin.DkpLib;
+using DkpWeb.Models;
 
 namespace DkpWeb.Controllers
 {
@@ -19,10 +20,63 @@ namespace DkpWeb.Controllers
             return View(mData.Transactions.OrderByDescending(t => t.Created));
         }
 
+        [Authorize(Roles = "DKP")]
+        public ActionResult Add()
+        {
+            ViewBag.People = mData.People.Where(p => !p.IsDeleted).ToList();
+            return View();
+        }
+
+        [HttpPost]
+        [Authorize(Roles = "DKP")]
+        public ActionResult Add(NewTransactionModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                Person debtor = null, creditor = null;
+                if (model.Creditor == model.Debtor)
+                {
+                    ModelState.AddModelError("", "The debtor and creditor must not be the same person.");
+                }
+                else
+                {
+                    debtor = mData.People.Where(p => p.ID == model.Debtor).SingleOrDefault();
+                    creditor = mData.People.Where(p => p.ID == model.Creditor).SingleOrDefault();
+                    if (debtor == null)
+                    {
+                        ModelState.AddModelError("Debtor", "Invalid debtor.");
+                    }
+                    if (creditor == null)
+                    {
+                        ModelState.AddModelError("Creditor", "Invalid creditor.");
+                    }
+                }
+
+
+                if (creditor != null && debtor != null)
+                {
+                    var trans = new Transaction();
+                    trans.CreditorID = creditor.ID;
+                    trans.DebtorID = debtor.ID;
+                    trans.Description = model.Description;
+                    trans.Amount = (int)Math.Round(model.Amount * 100d);
+                    trans.Created = DateTime.Now;
+                    trans.ID = Guid.NewGuid();
+                    mData.Transactions.InsertOnSubmit(trans);
+                    mData.SubmitChanges();
+                    return RedirectToAction("Index", "Transaction");
+                }
+            }
+
+            ViewBag.People = mData.People.Where(p => !p.IsDeleted).ToList();
+            return View(model);
+        }
+
         protected override void Dispose(bool disposing)
         {
             base.Dispose(disposing);
-            mData.Dispose();
+            if (disposing)
+                mData.Dispose();
         }
     }
 }
---
+++ DkpWeb/Models/TransactionModels.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+
+namespace DkpWeb.Models
+{
+    public class NewTransactionModel
+    {
+        [Required]
+        [StringLength(50, ErrorMessage = "The {0} must be at least {2} characters long.", MinimumLength = 1)]
+        [DataType(DataType.Text)]
+        //[Display(Name = "Description")]
+        public string Description { get; set; }
+
+        [Required]
+        [DataType(DataType.Currency)]
+        [Display(Name = "Amount (dollars)")]
+        [Range(0.01, 100)]
+        public double Amount { get; set; }
+
+        [Required]
+        //[Display(Name = "Debtor")]
+        public int Debtor { get; set; }
+
+        [Required]
+        //[Display(Name = "Creditor")]
+        public int Creditor { get; set; }
+    }
+}
