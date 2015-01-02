7e929808080863bb9501f0c246491717bd09929f
Austin Wise <AustinWise@gmail.com>
2015-01-02 14:34:15 -0800

    Added payment identity management.

--- DkpWeb/Controllers/PeopleController.cs
+++ DkpWeb/Controllers/PeopleController.cs
@@ -4,41 +4,82 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using Austin.DkpLib;
+using DkpWeb.Models;
 
 namespace DkpWeb.Controllers
 {
     public class PeopleController : Controller
     {
+        private DkpDataContext mData = new DkpDataContext();
+
+        protected override void Dispose(bool disposing)
+        {
+            base.Dispose(disposing);
+            if (disposing)
+                mData.Dispose();
+        }
+
         //
         // GET: /People/
 
         public ActionResult Index()
         {
-            using (var dc = new DkpDataContext())
-            {
-                return View(dc.ActivePeopleOrderedByName.ToArray());
-            }
+            return View(mData.ActivePeopleOrderedByName.ToArray());
         }
 
         public ActionResult All()
         {
-            using (var dc = new DkpDataContext())
-            {
-                return View("Index", dc.People
-                    .OrderBy(p => p.FirstName)
-                    .ThenBy(p => p.LastName)
-                    .ToArray());
-            }
+            return View("Index", mData.People
+                .OrderBy(p => p.FirstName)
+                .ThenBy(p => p.LastName)
+                .ToArray());
         }
 
         public ActionResult Detail(int id)
         {
-            Person person;
-            using (var dc = new DkpDataContext())
+            return View(mData.People.Where(p => p.ID == id).Single());
+        }
+
+        [HttpPost]
+        [Authorize(Roles = "Admin")]
+        public ActionResult DeletePaymentIdentity(int id)
+        {
+            var ident = mData.PaymentIdentities.Single(i => i.Id == id);
+            mData.PaymentIdentities.DeleteOnSubmit(ident);
+            mData.SubmitChanges();
+            return RedirectToAction("Detail", new { id = ident.PersonID });
+        }
+
+        [Authorize(Roles = "Admin")]
+        public ActionResult AddPaymentIdentity(int id)
+        {
+            ViewBag.Methods = mData.PaymentMethods;
+            ViewBag.Person = mData.People.Single(p => p.ID == id);
+            return View();
+        }
+
+        [Authorize(Roles = "Admin")]
+        [HttpPost]
+        public ActionResult AddPaymentIdentity(int id, AddPaymentMethodModel model)
+        {
+            var person = mData.People.Single(p => p.ID == id);
+
+            if (ModelState.IsValid)
             {
-                person = dc.People.Where(p => p.ID == id).Single();
+                var ident = new PaymentIdentity()
+                {
+                    PaymentMethod = mData.PaymentMethods.Single(m => m.Id == model.PaymentMethodId),
+                    Person = person,
+                    UserName = model.UserName.Trim(),
+                };
+                mData.PaymentIdentities.InsertOnSubmit(ident);
+                mData.SubmitChanges();
+                return RedirectToAction("Detail", new { id = id });
             }
-            return View(person);
+
+            ViewBag.Methods = mData.PaymentMethods;
+            ViewBag.Person = person;
+            return View();
         }
     }
 }
---
+++ DkpWeb/Models/PeopleModels.cs
@@ -0,0 +1,20 @@
+﻿using Austin.DkpLib;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace DkpWeb.Models
+{
+    public class AddPaymentMethodModel
+    {
+        [Required]
+        [Display(Name = "Payment Method")]
+        public int PaymentMethodId { get; set; }
+
+        [Required(AllowEmptyStrings = false)]
+        [Display(Name = "User Name")]
+        public string UserName { get; set; }
+    }
+}
