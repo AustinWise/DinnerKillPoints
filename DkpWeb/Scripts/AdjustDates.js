055d50dc973d85ee36500dbd48bd91a76182fd92
Austin Wise <AustinWise@gmail.com>
2016-02-21 14:31:41 -0800

    Remove extra stuff.

--- DkpWeb/Scripts/AdjustDates.js
+++ DkpWeb/Scripts/AdjustDates.js
@@ -1,6 +1,5 @@
 ﻿"use strict";
 $(document).ready(function () {
-    console.log('lol');
     $('.date-adjust').each(function (ndx, el) {
         var year = new Number(el.getAttribute('data-date-year'));
         var month = new Number(el.getAttribute('data-date-month')) - 1;
@@ -9,9 +8,7 @@ $(document).ready(function () {
         var minute = new Number(el.getAttribute('data-date-minute'));
         var second = new Number(el.getAttribute('data-date-second'));
         var milliOffset = new Number(el.getAttribute('data-milli'));
-        //var milliOffset = Date.UTC(year, month, day, hour, minute, second, 0);
         var newDate = new Date(milliOffset);
         el.innerHTML = newDate.toLocaleString();
-        //el.innerText = newDate.getDate().toString();
     });
 });
