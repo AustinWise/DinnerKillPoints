190ac313c407d817b6cf5693850bef1e2033e52a
Austin Wise <AustinWise@gmail.com>
2016-07-23 02:36:23 -0700

    Add the analyse feature.

---
+++ NewDkp/src/DkpWeb/Controllers/AnalyseController.cs
@@ -0,0 +1,50 @@
+﻿using System.Linq;
+using Austin.DkpLib;
+using System.IO;
+using Microsoft.AspNetCore.Mvc;
+using DkpWeb.Data;
+using DkpWeb.Models.AnalyzeViewModels;
+
+namespace DkpWeb.Controllers
+{
+	public class AnalyseController : Controller
+    {
+		ApplicationDbContext dc;
+
+		public AnalyseController(ApplicationDbContext db)
+		{
+			this.dc = db;
+		}
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+                dc.Dispose();
+            base.Dispose(disposing);
+        }
+
+        public ActionResult Index()
+        {
+            return View(dc.ActivePeopleOrderedByName);
+        }
+
+        public ActionResult Display(int[] peopleIds)
+        {
+            var people = peopleIds
+                .Select(i => dc.Person.Where(p => p.Id == i).Single())
+                .ToArray();
+            var swLog = new StringWriter();
+            var netMoney = DebtGraph.TestAlgo(dc, people, true, swLog);
+            
+            var swGraph = new StringWriter();
+            DebtGraph.WriteGraph(netMoney, swGraph);
+            var svg = DebtGraph.RenderGraphAsSvg(swGraph.ToString());
+
+            var mod = new AnalyseModel();
+            mod.LogOutput = swLog.ToString();
+            mod.ImageSvg = svg;
+            mod.Debtors = DebtGraph.GreatestDebtor(netMoney);
+            return View(mod);
+        }
+    }
+}
---
+++ NewDkp/src/DkpWeb/Models/AnalyzeViewModels/AnalyseModels.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using Microsoft.AspNetCore.Html;
+
+namespace DkpWeb.Models.AnalyzeViewModels
+{
+	public class AnalyseModel
+    {
+        [Required]
+        public string LogOutput { get; set; }
+
+        [Required]
+        public HtmlString ImageSvg { get; set; }
+
+        [Required]
+        public List<Tuple<Person, int>> Debtors { get; set; }
+    }
+}
