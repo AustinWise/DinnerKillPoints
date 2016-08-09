cc02981a74e45db2c1aea6367237e6857633c1e8
Austin Wise <AustinWise@gmail.com>
2016-08-09 00:16:03 -0700

    Create roles on startup if they don't exist. Not sure if this is a good idea.

--- NewDkp/src/DkpWeb/Program.cs
+++ NewDkp/src/DkpWeb/Program.cs
@@ -4,6 +4,10 @@ using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.AspNetCore.Identity;
+using DkpWeb.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 
 namespace DkpWeb
 {
@@ -18,7 +22,18 @@ namespace DkpWeb
                 .UseStartup<Startup>()
                 .Build();
 
+            var roles = host.Services.GetService<RoleManager<IdentityRole>>();
+            EnsureRole(roles, "Admin").Wait();
+            EnsureRole(roles, "DKP").Wait();
+
             host.Run();
+        }
+
+        static async Task EnsureRole(RoleManager<IdentityRole> roles, string name)
+        {
+            var role = await roles.FindByNameAsync(name);
+            if (role == null)
+                await roles.CreateAsync(new IdentityRole(name));
         }
     }
 }
