e689d2ab232dbfc0789a3b1797cc89dee0425e0d
Austin Wise <AustinWise@gmail.com>
2017-08-17 12:08:02 -0700

    Remove dead code in data calculation.

--- src/DkpWeb/wwwroot/js/site.js
+++ src/DkpWeb/wwwroot/js/site.js
@@ -2,12 +2,6 @@
 "use strict";
 $(document).ready(function () {
     $('.date-adjust').each(function (ndx, el) {
-        var year = new Number(el.getAttribute('data-date-year'));
-        var month = new Number(el.getAttribute('data-date-month')) - 1;
-        var day = new Number(el.getAttribute('data-date-day'));
-        var hour = new Number(el.getAttribute('data-date-hour'));
-        var minute = new Number(el.getAttribute('data-date-minute'));
-        var second = new Number(el.getAttribute('data-date-second'));
         var milliOffset = new Number(el.getAttribute('data-milli'));
         var newDate = new Date(milliOffset);
         el.innerHTML = newDate.toLocaleString();
