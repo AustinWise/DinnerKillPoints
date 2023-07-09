4939abdbf4591e8880ea3d8e13f1efa7f303c040
Austin Wise <AustinWise@gmail.com>
2023-07-09 09:56:46 -0700

    Add data export

---
+++ src/DkpWeb/Data/DataImportExport.cs
@@ -0,0 +1,20 @@
+﻿using System.IO;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+
+namespace DkpWeb.Data
+{
+    static class DataImportExport
+    {
+        public static void ExportData(ApplicationDbContext db, string path)
+        {
+            var options = new JsonSerializerOptions()
+            {
+                ReferenceHandler = ReferenceHandler.Preserve,
+                
+            };
+            string jsonString = JsonSerializer.Serialize(db, options);
+            File.WriteAllText(path, jsonString);
+        }
+    }
+}
